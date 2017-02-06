510324da3f28c32621dcf29ce1632610ae54b401
ChanYvonne <ychan1352@gmail.com>
2017-02-06 01:13:47 -0500

    finished! it's the SMPTE image pattern

--- picmaker.py
+++ picmaker.py
@@ -3,27 +3,72 @@
 def getImage(x, y):
 
     num = "P3 %d %d 255\n"%(x,y)
+    
+    for k in range(x):
+        for j in range(y):
 
-    j=0
-    k=0
-
-    while j < 500:
-        while k < 500:
-
-            r = 255;
-            g = 255;
-            b = 0;
-            
-            j+=1
-            k+=1
-
+            if j < 100:
+                r = 255
+                g = 255
+                b = 255
+                if k > 600:
+                    r = 0
+                    g = 0
+                    b = 255
+            if j >= 100 and j < 200:
+                r = 255
+                g = 255
+                b = 0
+                if k > 600:
+                    r = 255
+                    g = 0
+                    b = 255
+            if j >= 200 and j < 300:
+                r = 0
+                g = 255
+                b = 255
+                if k > 600:
+                    r = 255
+                    g = 255
+                    b = 0
+            if j >= 300 and j < 400:
+                r = 0
+                g = 255
+                b = 0
+                if k > 600:
+                    r = 255
+                    g = 0
+                    b = 0
+            if j >= 400 and j < 500:
+                r = 255
+                g = 0
+                b = 255
+                if k > 600:
+                    r = 0
+                    g = 255
+                    b = 255
+            if j >= 500 and j < 600:
+                r = 255
+                g = 0
+                b = 0
+                if k > 600:
+                    r = 0
+                    g = 0
+                    b = 0
+            if j >= 600:
+                r = 0
+                g = 0
+                b = 255
+                if k > 600:
+                    r = 255
+                    g = 255
+                    b = 255
+                
             num += "%d %d %d \n"%(r,g,b)
-    num += "\n"
-
+    
     return num
 
-    
-if __name__ == '__main__':
-    stuff = open("image.ppm", "w")
-    stuff.write(getImage(500,500))
-    stuff.close()
+#print getImage(500,500)
+stuff = open("image.ppm", "w")
+stuff.write(getImage(700,700))
+stuff.close()
